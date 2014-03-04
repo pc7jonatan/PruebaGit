76c8d63a249736c18a477dcc54963f7047b43f06
jonatan <pc7jonatan@gmail.com>
2014-03-05 00:13:38 +0100

    Primer Commit del proyecto

---
+++ PruebaGit/src/pruebagit/PruebaGit.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package pruebagit;
+
+/**
+ * @author Jonatan
+ *
+ */
+public class PruebaGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Prueba de EGit en Eclipse.");
+
+	}
+
+}
